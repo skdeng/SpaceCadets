2a0d0602d6b10db48985a7c1a75a1cfcf8769403
Faraz Oman <faraz@farazoman.com>
2016-02-28 23:21:30 -0500

    Inventory backend code is done, needs testing and actual GUI implementation
    I added Item as an abstract class as it works better for that type of
    object and we can use polymorphism especially in the inventory. abstract
    use() is in item therefore, consume and use as methods will not exist in
    the item children. (well use() exists but anyways). I made a new scene
    apparently when I started working. I added code to the assets. And shit
    needs to be improved. The only thing that may not have been properly
    implemented is the insert (2.4.2 because we have not yet gotten to GUIs
    so I'll put that down in the sheet).

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public sealed class Inventory : MonoBehaviour {
+	private int nMaxSpace;
+	private int nCount;
+	private List<Item> inventory;
+
+	private static Inventory instance = null;
+	private static readonly object padlock = new object();
+
+	private const int MAXSPACE = 10;
+
+	private Inventory()
+	{
+		nMaxSpace = 	MAXSPACE;
+		nCount = 0;
+		inventory = new List<Item>();
+	} 
+
+	/* private Inventory(int space)
+    {
+    	nMaxSpace = space;
+    	nCount = 0;
+    	List<Item> = new List<Item>();
+    }
+    */
+
+	public static Inventory Instance{
+		get
+		{
+			lock (padlock)
+			{
+				if (instance == null)
+				{
+					instance = new Inventory();
+				}
+				return instance;
+			}
+		}
+	}
+
+	public bool add(Item anItem){
+		if (!(nMaxSpace >= nCount)) {
+			inventory.Add (anItem);
+			return true;
+		}
+		return false;
+	}
+
+	public bool remove(Item anItem) {
+		if(!inventory.Remove(anItem)){
+			/*if(!inventory.Exists(new Predicate<Item>(anItem))){
+				throw new ArgumentException("Hey bud, that Item is not in the inventory at this time. Some shit must've gone wrong");
+			}*/
+			return false;
+		}
+
+		return true;
+	}
+
+
+}
+
