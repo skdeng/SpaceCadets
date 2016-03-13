a08f4dcc8e84ff3b8816dc04bd2f06cd1fcb3f08
Faraz Oman <faraz@farazoman.com>
2016-03-13 19:01:32 -0400

    Finalized animals, made trees & they grow
    Need to still make the growth as described in the diagrams

---
+++ Assets/Animals/PB_Spider/SpiderWalk.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpiderWalk : MonoBehaviour {
+
+
+	Animator anim;
+	//int idleHash = Animator.StringToHash("idle");
+	//int idleWalk = Animator.StringToHash("walk");
+	// Use this for initialization
+	int die = Animator.StringToHash("dead");
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+		//anim.Play ("Allosaurus_Run");
+		//anim.SetBool("IsWalking", true);
+		anim.SetTrigger (die);
+
+	}
+}
---
+++ Assets/Free_SpeedTrees/Conifer_Desktop Materials/treeGrowth.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class treeGrowth : MonoBehaviour {
+
+	void Start(){
+
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 curSize = transform.localScale;
+		if (curSize.x <= 4) {
+			transform.localScale += new Vector3 (0.00001F, 0.00001F, 0.00001F);
+
+		}
+
+	}
+}
