1e56e4506f37224760991febfc582786b20c5fb5
Joe Steele <joe.steele60@gmail.com>
2016-03-13 18:29:10 -0400

    added gravity and jump mechanic

--- Assets/Scripts/Player/FirstPersonController.cs
+++ Assets/Scripts/Player/FirstPersonController.cs
@@ -3,10 +3,14 @@ using System.Collections;
 
 public class FirstPersonController : MonoBehaviour {
 
+	//class fields
 	public float moveSpeed = 10.0f;
 	public float mouseSensitivity = 5.0f;
-	float pitchRotation = 0;
 	public float pitchRange = 60.0f;
+	public float jumpSpeed = 20.0f;
+	float pitchRotation = 0;
+	float vertVelocity = 0;
+
 
 	// Use this for initialization
 	void Start () {
@@ -16,6 +20,10 @@ public class FirstPersonController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+		//output
+		CharacterController cc = GetComponent<CharacterController> ();
+
+
 		//controlling player view
 		float rotateYaw = Input.GetAxis ("Mouse X") * mouseSensitivity;
 		transform.Rotate (0, rotateYaw, 0);
@@ -23,15 +31,21 @@ public class FirstPersonController : MonoBehaviour {
 		pitchRotation = Mathf.Clamp (pitchRotation, -pitchRange, pitchRange);
 		Camera.main.transform.localRotation = Quaternion.Euler (pitchRotation, 0, 0);
 
-		//user movement input
+		//user movement
 		float sideSpeed = Input.GetAxis ("Horizontal")* moveSpeed;
-		float forwardSpeed = Input.GetAxisRaw ("Vertical")* moveSpeed;
+		float forwardSpeed = Input.GetAxis("Vertical")* moveSpeed;
 
-		//speed
-		Vector3 aMovement = new Vector3 (sideSpeed, 0, forwardSpeed);
+		//jumping
+		if(cc.isGrounded && Input.GetButtonDown("Jump")){
+			vertVelocity = jumpSpeed;
+		}
+
+		//speed and gravity
+		vertVelocity += Physics.gravity.y * Time.deltaTime;
+		Vector3 aMovement = new Vector3 (sideSpeed, vertVelocity, forwardSpeed);
 		aMovement = transform.rotation * aMovement;
+		
 
-		CharacterController cc = GetComponent<CharacterController> ();
-		cc.SimpleMove(aMovement);
+		cc.Move(aMovement * Time.deltaTime);
 	}
 }
