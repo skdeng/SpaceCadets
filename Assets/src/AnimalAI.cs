431bca2b443ad652b3e823b0e24d3d8608258a37
Shao Kun Deng <deng_shaokun@hotmail.com>
2016-03-13 23:43:15 -0400

    Start animal and lifeform script

---
+++ Assets/src/AnimalAI.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Animal control
+public class AnimalAI : MonoBehaviour{
+
+	public enum BEHAVIOUR {
+		AGRESSOVE,
+		PASSIVE
+	}
+
+	public BEHAVIOUR behaviour;
+	public float speed;
+	public float damage;
+
+	//TODO add reference to player and spawner and other shits
+
+	// Use this for initialization
+	void Start () {
+		//link other objs this class depends on
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/src/Lifeform.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+
+
+//Abstract class for all lifeforms
+public abstract class Lifeform {
+	private int nCurrentHealth;
+	private int nMaxHealth;
+}
