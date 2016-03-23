01462df21af93bf62dc16ba77fc5dd4d7aab97a1
Joe Steele <joe.steele60@gmail.com>
2016-03-23 15:17:51 -0400

    added plasma fire mechanic, with basic particle system on collision

---
+++ Assets/Scripts/RigidBodies/Detonation.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Detonation : MonoBehaviour {
+
+	public float fLifeSpan = 3.0f;
+	public GameObject gFlare;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//Called on collision with another rigidbody 
+	void OnCollisionEnter(Collision collision){
+		Instantiate (gFlare, transform.position, Quaternion.identity);
+		Destroy (gameObject);
+	}
+
+	private void Explode(){
+		Destroy (gameObject);
+	}
+		
+}
