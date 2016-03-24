4ce3ded989de196e3367481527518d4d21b6ec30
stanleysu <stanley_su@live.com>
2016-03-23 22:41:54 -0400

    added startscreen and item selection

---
+++ Assets/InventorySelector.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class InventorySelector : MonoBehaviour {
+	// Use this for initialization
+	int pos = 0;
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.O) && pos > 0) {
+			Debug.Log ("Left");
+			transform.Translate(new Vector3(-58,0,0));
+			pos--;
+		}
+		if (Input.GetKeyDown(KeyCode.P) && pos < 7) {
+			Debug.Log ("Right");
+			transform.Translate(new Vector3(58,0,0));
+			pos++;
+		}
+
+	}
+}
---
+++ Assets/menuScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class menuScript : MonoBehaviour {
+
+	public string levelToLoad;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("space")) {
+			Application.LoadLevel(levelToLoad);
+		}
+	}
+}
