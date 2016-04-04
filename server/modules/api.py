c982f825de5f8122370435af3ce4b1e57805f30d
stanleysu <stanley_su@live.com>
2016-04-04 18:42:51 -0400

    Added addBid and getAllBids to api

--- server/modules/api.py
+++ server/modules/api.py
@@ -1,5 +1,8 @@
 import json
 import psycopg2
+import random
+import string
+import time
 from flask import Flask, request, redirect, url_for, send_from_directory,jsonify
 from werkzeug import secure_filename
 
@@ -16,13 +19,15 @@ def hello_world():
 @app.route('/getUserInfo', methods=['GET'])
 def getUserInfo():
   username = request.args.get('username')
-  cur = connectToDatabase()
+  connections = connectToDatabase()
+  conn = connections[0]
+  cur = connections[1]
   try:
 	cur.execute("""SELECT * FROM Users WHERE UserName = '"""+username+"""';""")
 	rows = cur.fetchall()
 	print rows[0]
   except:
- 	print "I can't drop our test database!"
+ 	print "I can't get user info!"
   table = {}
   table["username"] = rows[0][0];
   table["positionx"] = rows[0][1];
@@ -31,7 +36,75 @@ def getUserInfo():
   table["currentHealth"] = rows[0][4];
   table["inventoryItems"] = rows[0][5];
   
+  cur.close()
+  conn.close()
   return jsonify(table)
+
+#put an item up for auction
+@app.route('/addToAuction', methods=['GET'])
+def addToAuction():
+  username = request.args.get('username')
+  itemId = request.args.get('itemId')
+  connections = connectToDatabase()
+  conn = connections[0]
+  cur = connections[1]
+  print """INSERT INTO AuctionHouse Values ('"""+id_generator()+"""', '"""+username+"""','"""+(time.strftime("%Y-%m-%d"))+"""', '"""+itemId+"""');"""
+
+  try:
+	cur.execute("""INSERT INTO AuctionHouse Values ('"""+id_generator()+"""', '"""+username+"""','"""+(time.strftime("%Y-%m-%d"))+"""', '"""+itemId+"""');""")
+  	conn.commit()
+  	return "Item successfully added to AuctionHouse"
+  except:
+ 	return "Error inserting value to auctionhouse"
+  cur.close()
+  conn.close()
+
+#put an bid to item to an auction
+@app.route('/addBid', methods=['GET'])
+def addToBid():
+  auctionId = request.args.get('auctionId')
+  itemsTrade = request.args.get('itemsToTrade')
+  buyerName = request.args.get('buyerName')
+  connections = connectToDatabase()
+  conn = connections[0]
+  cur = connections[1]
+  print """INSERT INTO Bid Values ('"""+auctionId+"""', '"""+itemsTrade+"""','"""+buyerName+"""');"""
+
+  try:
+	cur.execute("""INSERT INTO Bid Values ('"""+auctionId+"""', '"""+itemsTrade+"""','"""+buyerName+"""');""")
+  	conn.commit()
+  	return "Item successfully added to Bid"
+  except:
+ 	return "Error inserting value to Bid"
+  cur.close()
+  conn.close()
+
+#Get all the bids of an auction
+@app.route('/getAllBids', methods=['GET'])
+def getAllBids():
+  auctionId = request.args.get('auctionId')
+  connections = connectToDatabase()
+  conn = connections[0]
+  cur = connections[1]
+  try:
+	cur.execute("""SELECT * FROM Bid WHERE auctionId = '"""+auctionId+"""';""")
+	rows = cur.fetchall()
+  except:
+ 	print "I can't get user info!"
+  output = {}
+  table = []
+  for num in range (0, len(rows)-1):
+    bid = {}
+    bid["auctionId"] = rows[num][0]
+    bid["buyerName"] = rows[num][2]
+    bid["itemsToTrade"] = rows[num][1]
+    table.append(bid)
+  output["bids"] = table
+  print len(rows)
+
+  cur.close()
+  conn.close()
+  return jsonify(output)
 
 def connectToDatabase():
   username = request.args.get('username')
@@ -44,7 +117,9 @@ def connectToDatabase():
     print param
 
   cur = conn.cursor()
-  return cur
+  return [conn,cur]
 
+def id_generator(size=10, chars=string.ascii_uppercase + string.digits):
+  return ''.join(random.choice(chars) for _ in range(size))
 if __name__ == '__main__':
   app.run(host='0.0.0.0')
