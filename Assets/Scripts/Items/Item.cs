7b9dbd2976f17beef7159af2184a42690ead4751
Joe Steele <joe.steele60@gmail.com>
2016-04-09 18:19:18 -0400

    added Faraz's item backend to branch

---
+++ Assets/Scripts/Items/Item.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public abstract class Item : MonoBehaviour {
+
+	//TODO maybe making a struct for the stats will make it easy for passing?
+
+	protected int weight;
+	protected string name;
+	protected GUISkin itemImage; //TODO do we need a 2d for inventory & a 3D for world play"?
+
+	public abstract bool use (); //TODO maybe a void? not sure bout dis yet
+
+	public int getWeight(){
+		if (weight <= 0) {
+			weight = 1;
+		}
+		return weight;
+	}
+
+	public void setWeight(int newWeight){
+		if (newWeight <= 0) {
+			weight = 1;
+		}
+		weight = newWeight;
+	}
+
+	public void setName(string newName){
+		if(newName.Equals("")){
+			name = "No Name";
+		}
+
+		name = newName;
+	}
+
+	public string getName(){
+		if (name.Equals ("")) {
+			name = "No Name";
+		}
+
+		return name;
+	}
+
+	protected void setSkin(GUISkin newSkin){
+		if (newSkin == null) {
+			//TODO add some default item skin to this thingy
+		}
+
+		itemImage = newSkin;
+	}
+
+	public GUISkin getSkin(){
+		if (itemImage == null) {
+			//TODO some default thing here
+		}
+
+		return itemImage;
+	}
+
+
+}
---
+++ Assets/Scripts/Items/ItemDB.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ItemDB : MonoBehaviour {
+	public List<Item> itemList;
+	private Inventory inventory;
+
+	// Use this for initialization
+	void Start () {
+		inventory = GameObject.FindGameObjectWithTag ("Inventory").GetComponent<Inventory> ();
+
+	}
+
+	void OnGUI(){
+		for (int i = 0; i < 10; i++) {
+			GUI.Label (new Rect (10, 10 + i*20, 200, 50), "Hello");
+		}
+		print ("Helloworld");
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
